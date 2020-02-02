ec47f0d726f7f5f3ae33c920d0f9b40d5bc0d119
taroj khan <tarujkhan@yahoo.com>
2020-02-02 16:29:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,7 +37,7 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil painting" => 3,
+                  "oil paintings" => 3,
                   "muskets" => 2,
                   "gun powder" => 4
                   
